b6fa3980423da65719e305e1c4b9245eb39191f1
Quang Dang <quang.dang@homa.company>
2020-04-18 01:44:32 +0700

    Comment, Like Btn Component

---
+++ src/components/Shared/CommentButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="like-icon">
+     <v-icon size="20px" color="#64B5F6">mdi-comment-text-multiple</v-icon>
+      <span class="comment-icon-content"> {{ comments }} </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['comments']
+};
+</script>
+
+<style scoped>
+  div {
+    position: relative;
+  }
+  
+  span {
+    position: absolute !important;
+    top: 10px;
+    left: 22px;
+    font-size: 12px;
+  }
+
+</style>
---
+++ src/components/Shared/LikeButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="like-icon">
+    <v-icon size="20px" color="#64B5F6" >mdi-thumb-up</v-icon>
+    <span class="like-icon-content">{{ likes }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['likes']
+};
+</script>
+
+<style scoped>
+  div {
+    
+    position: relative;
+  }
+
+  span {
+    position: absolute !important;
+    top: 10px;
+    left: 22px;
+    font-size: 12px;
+  }
+
+</style>
