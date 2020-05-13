df033164a2cfa9ac547c326ab96beacb1c2d65aa
Quang Dang <quang.dang@homa.company>
2020-05-14 00:51:56 +0700

    resolve conflict

--- src/components/Post/Audio/Song/SongList.vue
+++ src/components/Post/Audio/Song/SongList.vue
@@ -88,12 +88,7 @@
           :data="mostViewBlogs.data"
         />
 
-        <side-card
-          class="fix-sidebar"
-          :title="tags.title"
-          :type="tags.type"
-          :data="tags.data"
-        />
+        <side-card class="fix-sidebar" :title="tags.title" :type="tags.type" :data="tags.data" />
 
         <side-card
           class="fix-sidebar member-online"
@@ -107,173 +102,159 @@
 </template>
 
 <script>
+import SideCard from "@/components/Shared/SideCard";
 
-import SideCard from '@/components/Shared/SideCard';
-
-import Audio from './Song';
+import Audio from "./Song";
 
 import { mapState, mapActions } from "vuex";
 
 export default {
   components: {
     song: Audio,
-    SideCard,
+    SideCard
   },
   data() {
     return {
       showViewMoreBtn: true,
       topBloggers: {
-        title: 'Top Bloggers',
+        title: "Top Bloggers",
         type: 1,
         data: [
           {
-            _id: '1',
+            _id: "1",
             icon:
-              'https://res.cloudinary.com/hongquangraem/image/upload/v1587030274/Draw-io-trophies/--02-128_kotkpp.png',
-            text: 'chau_chau',
+              "https://res.cloudinary.com/hongquangraem/image/upload/v1587030274/Draw-io-trophies/--02-128_kotkpp.png",
+            text: "chau_chau"
           },
           {
-            _id: '2',
+            _id: "2",
             icon:
-              'https://res.cloudinary.com/hongquangraem/image/upload/v1587030285/Draw-io-trophies/advantage_quality-128_hxdkdz.png',
-            text: 'nhat_anh',
+              "https://res.cloudinary.com/hongquangraem/image/upload/v1587030285/Draw-io-trophies/advantage_quality-128_hxdkdz.png",
+            text: "nhat_anh"
           },
           {
-            _id: '3',
+            _id: "3",
             icon:
-              'https://res.cloudinary.com/hongquangraem/image/upload/v1587030256/Draw-io-trophies/movie-10-128_yf3ng3.png',
-            text: 'thanh_ton',
-          },
-        ],
+              "https://res.cloudinary.com/hongquangraem/image/upload/v1587030256/Draw-io-trophies/movie-10-128_yf3ng3.png",
+            text: "thanh_ton"
+          }
+        ]
       },
       tags: {
-        title: 'Tags',
+        title: "Tags",
         type: 2,
         data: [
           {
-            _id: '1',
-            text: 'javascript',
-            counter: 153,
+            _id: "1",
+            text: "javascript",
+            counter: 153
           },
           {
-            _id: '2',
-            text: 'discussion',
-            counter: 153,
+            _id: "2",
+            text: "discussion",
+            counter: 153
           },
           {
-            _id: '3',
-            text: 'nodejs',
-            counter: 153,
+            _id: "3",
+            text: "nodejs",
+            counter: 153
           },
           {
-            _id: '4',
-            text: 'html',
-            counter: 153,
-          },
-        ],
+            _id: "4",
+            text: "html",
+            counter: 153
+          }
+        ]
       },
       mostViewBlogs: {
-        title: 'Most Views',
+        title: "Most Views",
         type: 2,
         data: [
           {
-            _id: '1',
-            text: 'Javascript the best parts',
-            counter: 153,
+            _id: "1",
+            text: "Javascript the best parts",
+            counter: 153
           },
           {
-            _id: '2',
-            text: 'Top 5 nodejs frameworks',
-            counter: 100,
+            _id: "2",
+            text: "Top 5 nodejs frameworks",
+            counter: 100
           },
           {
-            _id: '3',
-            text: 'HTML for dummies',
-            counter: 99,
+            _id: "3",
+            text: "HTML for dummies",
+            counter: 99
           },
           {
-            _id: '4',
-            text: 'Testing issues',
-            counter: 80,
+            _id: "4",
+            text: "Testing issues",
+            counter: 80
           },
           {
-            _id: '5',
-            text: 'Setting Mongo local',
-            counter: 79,
+            _id: "5",
+            text: "Setting Mongo local",
+            counter: 79
           },
           {
-            _id: '6',
-            text: 'Javascript the best parts',
-            counter: 153,
+            _id: "6",
+            text: "Javascript the best parts",
+            counter: 153
           },
           {
-            _id: '7',
-            text: 'Top 5 nodejs frameworks',
-            counter: 100,
+            _id: "7",
+            text: "Top 5 nodejs frameworks",
+            counter: 100
           },
           {
-            _id: '8',
-            text: 'HTML for dummies',
-            counter: 99,
+            _id: "8",
+            text: "HTML for dummies",
+            counter: 99
           },
           {
-            _id: '9',
-            text: 'Testing issues',
-            counter: 80,
+            _id: "9",
+            text: "Testing issues",
+            counter: 80
           },
           {
-            _id: '10',
-            text: 'Setting Mongo local',
-            counter: 79,
-          },
-        ],
+            _id: "10",
+            text: "Setting Mongo local",
+            counter: 79
+          }
+        ]
       },
       membersOnline: {
-        title: 'Members Online',
+        title: "Members Online",
         type: 1,
         data: [
           {
-            _id: '1',
+            _id: "1",
             icon:
-              'https://res.cloudinary.com/hongquangraem/image/upload/v1586965772/Draw-io-avatars/12_avatar-128_hvhfyk.png',
-            text: 'ngo_minh',
+              "https://res.cloudinary.com/hongquangraem/image/upload/v1586965772/Draw-io-avatars/12_avatar-128_hvhfyk.png",
+            text: "ngo_minh"
           },
           {
-            _id: '2',
+            _id: "2",
             icon:
-              'https://res.cloudinary.com/hongquangraem/image/upload/v1586965680/Draw-io-avatars/4_avatar-128_rk2yxz.png',
-            text: 'nhat_anh',
+              "https://res.cloudinary.com/hongquangraem/image/upload/v1586965680/Draw-io-avatars/4_avatar-128_rk2yxz.png",
+            text: "nhat_anh"
           },
           {
-            _id: '3',
+            _id: "3",
             icon:
-              'https://res.cloudinary.com/hongquangraem/image/upload/v1586965594/Draw-io-avatars/avatar-128_s94fqh.png',
-            text: 'thanh_ton',
-          },
-        ],
+              "https://res.cloudinary.com/hongquangraem/image/upload/v1586965594/Draw-io-avatars/avatar-128_s94fqh.png",
+            text: "thanh_ton"
+          }
+        ]
       },
       showTopBloggers: true,
       showTags: true,
       showMostViewBlogs: true,
       showMembersOnline: true,
       sideBarStyle: {
-        paddingTop: '12px',
-      },
+        paddingTop: "12px"
+      }
     };
   },
-
-  created() {
-    if (this.$route.path === '/stream/songs') {
-      this.showTitlePage = true;
-      this.showViewMoreBtn = false;
-      this.sideBarStyle.paddingTop = '5px';
-      this.showTopBloggers = false;
-    }
-
-    if (this.$route.path === '/stream' || this.$route.path === '/') {
-      this.mostViewBlogs.title = 'Top 5 songs';
-      const sliceMostViews = this.mostViewBlogs.data.slice(5);
-
   methods: {
     ...mapActions("songs", ["getSongs"])
   },
@@ -285,15 +266,14 @@ export default {
       this.mostViewBlogs.data = sliceMostViews;
     }
 
-    await this.getSongs()
+    await this.getSongs();
   },
   computed: {
-    ...mapState('utils', ['errorMes', 'isLoading']),
-    ...mapState('stream', ['newestSongs']),
+    ...mapState("utils", ["errorMes", "isLoading"]),
+    ...mapState("stream", ["newestSongs"]),
     songs() {
       return this.newestSongs;
-    },
-  },
+    }
   },
   watch: {
     isLoading(newVal) {
@@ -305,7 +285,6 @@ export default {
       }
     }
   }
-
 };
 </script>
 
