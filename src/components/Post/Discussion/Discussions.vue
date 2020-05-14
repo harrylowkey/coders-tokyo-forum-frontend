182cf7c489d6d26c62449a850e4365b472797469
Quang Dang <quang.dang@homa.company>
2020-05-14 09:42:05 +0700

    implement load more discussions when scroll

--- src/components/Post/Discussion/Discussions.vue
+++ src/components/Post/Discussion/Discussions.vue
@@ -1,84 +1,85 @@
 <template>
-  <v-container class='pt-0'>
+  <v-container class="pt-0">
     <v-row>
-      <v-col cols='12' sm='7' md='8' lg='8' xl='7' offset-xl='1' class='pt-0'>
+      <v-col cols="12" sm="7" md="8" lg="8" xl="7" offset-xl="1" class="pt-0">
         <v-skeleton-loader
-          class='mt-5 mb-5'
-          v-if='isLoading'
-          type='list-item-avatar-three-line, list-item-three-line'
+          class="mt-5 mb-5"
+          v-if="isLoading"
+          type="list-item-avatar-three-line, list-item-three-line"
         ></v-skeleton-loader>
 
         <v-skeleton-loader
-          class='mt-5 mb-5'
-          v-if='isLoading'
-          type='list-item-avatar-three-line, list-item-three-line'
+          class="mt-5 mb-5"
+          v-if="isLoading"
+          type="list-item-avatar-three-line, list-item-three-line"
         ></v-skeleton-loader>
 
         <v-skeleton-loader
-          class='mt-5 mb-5'
-          v-if='isLoading'
-          type='list-item-avatar-three-line, list-item-three-line'
+          class="mt-5 mb-5"
+          v-if="isLoading"
+          type="list-item-avatar-three-line, list-item-three-line"
         ></v-skeleton-loader>
 
         <v-skeleton-loader
-          class='mt-5 mb-5'
-          v-if='isLoading'
-          type='list-item-avatar-three-line, list-item-three-line'
+          class="mt-5 mb-5"
+          v-if="isLoading"
+          type="list-item-avatar-three-line, list-item-three-line"
         ></v-skeleton-loader>
 
         <v-skeleton-loader
-          class='mt-5 mb-5'
-          v-if='isLoading'
-          type='list-item-avatar-three-line, list-item-three-line'
+          class="mt-5 mb-5"
+          v-if="isLoading"
+          type="list-item-avatar-three-line, list-item-three-line"
         ></v-skeleton-loader>
-        <div v-if='!isLoading'>
+        <div v-if="!isLoading">
           <discussion
-            v-for='item in discussions'
-            :key='item._id'
-            :_id='item._id'
-            :tags='item.tags'
-            :comments='item.comments'
-            :likes='item.likes'
-            :savedBy='item.savedBy'
-            :user='item.user'
-            :topic='item.topic'
-            :content='item.content'
-            :type='item.type'
-            :createdAt='item.createdAt'
-            :updatedAt='item.updatedAt'
-            :metadata='item.metadata'
+            v-for="item in discussions"
+            :key="item._id"
+            :_id="item._id"
+            :tags="item.tags"
+            :comments="item.comments"
+            :likes="item.likes"
+            :savedBy="item.savedBy"
+            :user="item.user"
+            :topic="item.topic"
+            :content="item.content"
+            :type="item.type"
+            :createdAt="item.createdAt"
+            :updatedAt="item.updatedAt"
+            :metadata="item.metadata"
           ></discussion>
         </div>
 
         <div
-          v-infinite-scroll='loadMore'
-          infinite-scroll-disabled='busy'
-          infinite-scroll-distance='10'
+          v-infinite-scroll="loadMore"
+          infinite-scroll-disabled="isLoadmore"
+          infinite-scroll-distance="10"
         ></div>
+        <v-text-field color="primary" v-if="isLoadmore" loading disabled />
       </v-col>
-      <v-col cols='12' sm='4' md='4' lg='4' xl='4' :style='sideBarStyle'>
+      <v-col cols="12" sm="4" md="4" lg="4" xl="4" :style="sideBarStyle">
         <side-card
-          class='fix-sidebar top-blogger'
-          :title='topBloggers.title'
-          :type='topBloggers.type'
-          :data='topBloggers.data'
-          v-if='showTopBloggers'
+          class="fix-sidebar top-blogger"
+          :title="topBloggers.title"
+          :type="topBloggers.type"
+          :data="topBloggers.data"
+          v-if="showTopBloggers"
         />
 
         <side-card
-          class='fix-sidebar most-view-posts'
-          :title='mostViewBlogs.title'
-          :type='mostViewBlogs.type'
-          :data='mostViewBlogs.data'
+          class="fix-sidebar most-view-posts"
+          :title="mostViewBlogs.title"
+          :type="mostViewBlogs.type"
+          :data="mostViewBlogs.data"
         />
 
-        <side-card class='fix-sidebar' :title='tags.title' :type='tags.type' :data='tags.data' />
+        <side-card class="fix-sidebar" :title="tags.title" :type="tags.type" :data="tags.data" />
 
         <side-card
-          class='fix-sidebar member-online'
-          :title='membersOnline.title'
-          :type='membersOnline.type'
-          :data='membersOnline.data'
+          class="fix-sidebar member-online"
+          :title="membersOnline.title"
+          :type="membersOnline.type"
+          :data="membersOnline.data"
         />
       </v-col>
     </v-row>
@@ -86,11 +87,11 @@
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex';
+import { mapState, mapActions } from "vuex";
 
-import SideCard from '@/components/Shared/SideCard';
+import SideCard from "@/components/Shared/SideCard";
 
-import Discussion from './Discussion';
+import Discussion from "./Discussion";
 
 export default {
   components: {
@@ -101,132 +102,132 @@ export default {
     return {
       showViewMoreBtn: true,
       topBloggers: {
-        title: 'Top Bloggers',
+        title: "Top Bloggers",
         type: 1,
         data: [
           {
-            _id: '1',
+            _id: "1",
             icon:
-              'https://res.cloudinary.com/hongquangraem/image/upload/v1587030274/Draw-io-trophies/--02-128_kotkpp.png',
-            text: 'chau_chau'
+              "https://res.cloudinary.com/hongquangraem/image/upload/v1587030274/Draw-io-trophies/--02-128_kotkpp.png",
+            text: "chau_chau"
           },
           {
-            _id: '2',
+            _id: "2",
             icon:
-              'https://res.cloudinary.com/hongquangraem/image/upload/v1587030285/Draw-io-trophies/advantage_quality-128_hxdkdz.png',
-            text: 'nhat_anh'
+              "https://res.cloudinary.com/hongquangraem/image/upload/v1587030285/Draw-io-trophies/advantage_quality-128_hxdkdz.png",
+            text: "nhat_anh"
           },
           {
-            _id: '3',
+            _id: "3",
             icon:
-              'https://res.cloudinary.com/hongquangraem/image/upload/v1587030256/Draw-io-trophies/movie-10-128_yf3ng3.png',
-            text: 'thanh_ton'
+              "https://res.cloudinary.com/hongquangraem/image/upload/v1587030256/Draw-io-trophies/movie-10-128_yf3ng3.png",
+            text: "thanh_ton"
           }
         ]
       },
       tags: {
-        title: 'Tags',
+        title: "Tags",
         type: 2,
         data: [
           {
-            _id: '1',
-            text: 'javascript',
+            _id: "1",
+            text: "javascript",
             counter: 153
           },
           {
-            _id: '2',
-            text: 'discussion',
+            _id: "2",
+            text: "discussion",
             counter: 153
           },
           {
-            _id: '3',
-            text: 'nodejs',
+            _id: "3",
+            text: "nodejs",
             counter: 153
           },
           {
-            _id: '4',
-            text: 'html',
+            _id: "4",
+            text: "html",
             counter: 153
           }
         ]
       },
       mostViewBlogs: {
-        title: 'Most Views',
+        title: "Most Views",
         type: 2,
         data: [
           {
-            _id: '1',
-            text: 'Javascript the best parts',
+            _id: "1",
+            text: "Javascript the best parts",
             counter: 153
           },
           {
-            _id: '2',
-            text: 'Top 5 nodejs frameworks',
+            _id: "2",
+            text: "Top 5 nodejs frameworks",
             counter: 100
           },
           {
-            _id: '3',
-            text: 'HTML for dummies',
+            _id: "3",
+            text: "HTML for dummies",
             counter: 99
           },
           {
-            _id: '4',
-            text: 'Testing issues',
+            _id: "4",
+            text: "Testing issues",
             counter: 80
           },
           {
-            _id: '5',
-            text: 'Setting Mongo local',
+            _id: "5",
+            text: "Setting Mongo local",
             counter: 79
           },
           {
-            _id: '6',
-            text: 'Javascript the best parts',
+            _id: "6",
+            text: "Javascript the best parts",
             counter: 153
           },
           {
-            _id: '7',
-            text: 'Top 5 nodejs frameworks',
+            _id: "7",
+            text: "Top 5 nodejs frameworks",
             counter: 100
           },
           {
-            _id: '8',
-            text: 'HTML for dummies',
-            counter: 99,
+            _id: "8",
+            text: "HTML for dummies",
+            counter: 99
           },
           {
-            _id: '9',
-            text: 'Testing issues',
+            _id: "9",
+            text: "Testing issues",
             counter: 80
           },
           {
-            _id: '10',
-            text: 'Setting Mongo local',
+            _id: "10",
+            text: "Setting Mongo local",
             counter: 79
           }
         ]
       },
       membersOnline: {
-        title: 'Members Online',
+        title: "Members Online",
         type: 1,
         data: [
           {
-            _id: '1',
+            _id: "1",
             icon:
-              'https://res.cloudinary.com/hongquangraem/image/upload/v1586965772/Draw-io-avatars/12_avatar-128_hvhfyk.png',
-            text: 'ngo_minh'
+              "https://res.cloudinary.com/hongquangraem/image/upload/v1586965772/Draw-io-avatars/12_avatar-128_hvhfyk.png",
+            text: "ngo_minh"
           },
           {
-            _id: '2',
+            _id: "2",
             icon:
-              'https://res.cloudinary.com/hongquangraem/image/upload/v1586965680/Draw-io-avatars/4_avatar-128_rk2yxz.png',
-            text: 'nhat_anh'
+              "https://res.cloudinary.com/hongquangraem/image/upload/v1586965680/Draw-io-avatars/4_avatar-128_rk2yxz.png",
+            text: "nhat_anh"
           },
           {
-            _id: '3',
+            _id: "3",
             icon:
-              'https://res.cloudinary.com/hongquangraem/image/upload/v1586965594/Draw-io-avatars/avatar-128_s94fqh.png',
-            text: 'thanh_ton'
+              "https://res.cloudinary.com/hongquangraem/image/upload/v1586965594/Draw-io-avatars/avatar-128_s94fqh.png",
+            text: "thanh_ton"
           }
         ]
       },
@@ -235,25 +236,27 @@ export default {
       showMostViewBlogs: true,
       showMembersOnline: true,
       sideBarStyle: {
-        paddingTop: '12px'
-      },
-      busy: false
+        paddingTop: "12px"
+      }
     };
   },
   computed: {
-    ...mapState('utils', ['errorMes', 'isLoading']),
-    ...mapState('discussions', ['discussions', 'metadata'])
+    ...mapState("utils", ["errorMes", "isLoading", "isLoadmore"]),
+    ...mapState("discussions", ["discussions", "metadata"])
   },
   methods: {
-    ...mapActions('discussions', ['getDiscussions']),
+    ...mapActions("discussions", ["getDiscussions", "loadMoreDiscussions"]),
     async loadMore() {
-      this.busy = true;
-      const res = await this.getDiscussions({});
+      if (this.metadata.page >= this.metadata.totalPage) {
+        return;
+      }
+
+      await this.loadMoreDiscussions({ page: this.metadata.page + 1 });
     }
   },
   async created() {
-    if (this.$route.path === '/stream' || this.$route.path === '/') {
-      this.mostViewBlogs.title = 'Top 5 Discussions';
+    if (this.$route.path === "/stream" || this.$route.path === "/") {
+      this.mostViewBlogs.title = "Top 5 Discussions";
       const sliceMostViews = this.mostViewBlogs.data.slice(5);
       this.mostViewBlogs.data = sliceMostViews;
     }
@@ -262,8 +265,8 @@ export default {
   errorMes(newVal) {
     if (newVal.length) {
       this.$notify({
-        type: 'error',
-        title: 'Update failed',
+        type: "error",
+        title: "Update failed",
         text: newVal
       });
     }
