7649d3bf65b75ebb0b53fc3e655e9b32acac75d2
Hong Quang <danghongquang99@gmail.com>
2020-06-08 09:38:26 +0700

    update config env

--- src/config.js
+++ src/config.js
@@ -1,10 +1,11 @@
-const path = require('path');
+require('dotenv').config();
 
-const mode = process.env.NODE_ENV || 'local';
-const _path = `${path.join(__dirname, '../../.env')}.${mode}`;
-require('dotenv').config({ path: _path });
+let BACKEND_URL = 'http://localhost:3000/api/v1';
+let SOCKET_URL = 'http://localhost:8888';
 
-const BACKEND_URL = `${process.env.BACKEND_URL}`;
-const SOCKET_URL = `${process.env.SOCKET_URL}`;
+if (process.env.VUE_APP_ENV === 'production') {
+  BACKEND_URL = 'http://34.87.169.3:3000/api/v1'
+  SOCKET_URL = 'http://34.87.169.3:8888'
+}
 
 export { BACKEND_URL, SOCKET_URL };
