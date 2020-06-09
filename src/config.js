c982f2f4c08cfc7bff6c21aeaa79e4a98093d084
Hong Quang <danghongquang99@gmail.com>
2020-06-09 20:53:05 +0700

    update config host

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 
-let BACKEND_URL = 'http://localhost:3000/api/v1';
-let SOCKET_URL = 'http://localhost:8888';
+let BACKEND_URL = 'http://34.87.169.3:3000/api/v1';
+let SOCKET_URL = 'http://34.87.169.3:8888';
 
 if (process.env.VUE_APP_ENV === 'production') {
-  BACKEND_URL = 'http://34.87.169.3:3000/api/v1';
-  SOCKET_URL = 'http://34.87.169.3:8888';
+  BACKEND_URL = 'http://10.146.0.2:3000/api/v1';
+  SOCKET_URL = 'http://10.146.0.2:8888';
 }
 
 export { BACKEND_URL, SOCKET_URL };
