786ee0a8628457bc14330327d1ef78d73e9d5437
Hong Quang <danghongquang99@gmail.com>
2020-06-14 10:56:37 +0700

    config url prod

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
-let BACKEND_URL = 'http://localhost:3000/api/v1';
-let SOCKET_URL = 'http://localhost:8888';
+let BACKEND_URL = 'http://34.87.169.3:3000/api/v1';
+let SOCKET_URL = 'http://34.87.169.3:8888';
 
 if (process.env.VUE_APP_ENV === 'production') {
   BACKEND_URL = 'http://10.146.0.2:3000/api/v1';
