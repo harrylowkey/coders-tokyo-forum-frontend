2ce63b4bfe53ce6e32a2ce359caf7cdce03c83c9
hong quang <danghongquang99@gmail.com>
2021-04-16 07:47:50 +0700

    edit config var

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
-let BACKEND_URL = 'https://54.151.207.118:3000/api/v1';
+let BACKEND_URL = 'https://54.151.207.118/api/v1';
 let SOCKET_URL = 'https://54.151.207.118:8888';
 
 if (process.env.VUE_APP_ENV === 'production') {
-  BACKEND_URL = 'http://172.31.40.8:3000/api/v1';
+  BACKEND_URL = 'http://172.31.40.8/api/v1';
   SOCKET_URL = 'http://172.31.40.8:8888';
 }
 
