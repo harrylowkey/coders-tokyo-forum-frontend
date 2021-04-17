440227163312ddd892380c612d195143c8e535c6
hong quang <danghongquang99@gmail.com>
2021-04-17 16:02:33 +0700

    update config.yml file

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 let BACKEND_URL = 'https://api.hongquangraem.cf/api/v1';
-let SOCKET_URL = 'https://api.hongquangraem.cf/ws';
+let SOCKET_URL = 'https://api.hongquangraem.cf';
 
 if (process.env.VUE_APP_ENV === 'production') {
   BACKEND_URL = 'http://172.31.40.8/api/v1';
